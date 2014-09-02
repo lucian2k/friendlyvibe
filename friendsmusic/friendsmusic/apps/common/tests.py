80efd12b5053518730aa1654ef628e55e17cd58a
Lucian Nicolescu <lucian@mashups.ro>
2014-09-02 23:40:30 +0300

    Small test for youtube ID extractor

--- friendsmusic/friendsmusic/apps/common/tests.py
+++ friendsmusic/friendsmusic/apps/common/tests.py
@@ -6,11 +6,13 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from tasks import _extract_youtube_id, check_video
 
+class TasksMethodsTest(TestCase):
+    def test_extract_youtube_id(self):
+        """
+        Tests that the ID is correctly extracted from various Youtube links
+        """
+        self.assertEqual(_extract_youtube_id('http://youtu.be/WBKnpyoFEBo'), 'WBKnpyoFEBo')
+        self.assertEqual(_extract_youtube_id('https://www.youtube.com/watch?v=Fkked2Wgd1g'), 'Fkked2Wgd1g')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
