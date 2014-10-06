c53d3aef76693dc46fa50c5e1561eb2bfe5282b4
Lucian Nicolescu <lucian@mashups.ro>
2014-10-06 23:56:32 +0300

    Added youtube synced field

--- friendsmusic/friendsmusic/apps/common/models.py
+++ friendsmusic/friendsmusic/apps/common/models.py
@@ -4,26 +4,28 @@ from django.contrib.auth.models import User
 
 PROVIDER_YOUTUBE = 1
 AVAIL_PROVIDERS = (
-        (PROVIDER_YOUTUBE, 'Youtube'),
-    )
+    (PROVIDER_YOUTUBE, 'Youtube'),
+)
+
 
 class Item(models.Model):
-	provider = models.SmallIntegerField(choices=AVAIL_PROVIDERS, db_index=True)
-	name = models.CharField(max_length=100)
-	source_identifier = models.CharField(max_length=100, null=True, unique=True)
+    provider = models.SmallIntegerField(choices=AVAIL_PROVIDERS, db_index=True)
+    name = models.CharField(max_length=100)
+    source_identifier = models.CharField(max_length=100, null=True, unique=True)
 
-	def __unicode__(self):
-		return u'Item id: %s, source id: %s' % (self.pk, self.source_identifier)
+    def __unicode__(self):
+        return u'Item id: %s, source id: %s' % (self.pk, self.source_identifier)
 
-	def to_json(self):
-		return {'title': self.name,
-				'code': self.source_identifier,
-				'permalink': self.permalink}
+    def to_json(self):
+        return {'title': self.name,
+                'code': self.source_identifier,
+                'permalink': self.permalink}
 
-	@property
-	def permalink(self):
-		if self.provider == PROVIDER_YOUTUBE:
-			return 'http://www.youtube.com/watch?v=%s' % self.source_identifier
+    @property
+    def permalink(self):
+        if self.provider == PROVIDER_YOUTUBE:
+            return 'http://www.youtube.com/watch?v=%s' % self.source_identifier
+
 
 class Playlist(models.Model):
     user = models.ForeignKey(User)
@@ -36,18 +38,21 @@ class Playlist(models.Model):
     is_private = models.BooleanField()
 
     def __unicode__(self):
-    	return u'Playlist ID: %s' % self.pk
+        return u'Playlist ID: %s' % self.pk
 
     def to_model(self):
         return {'title': self.youtube_pl_name,
                 'is_private': self.is_private}
 
-class PlaylistItem(models.Model):
-	wall_created = models.DateTimeField(db_index=True, auto_now=True)
-	playlist_obj = models.ForeignKey(Playlist)
-	item_obj = models.ForeignKey(Item)
 
-	def __unicode__(self):
-		return u'Playlist item: %s, user: %s' % (self.item_obj.source_identifier,
-												self.playlist_obj.user)
+class PlaylistItem(models.Model):
+    wall_created = models.DateTimeField(db_index=True, auto_now=True)
+    playlist_obj = models.ForeignKey(Playlist)
+    item_obj = models.ForeignKey(Item)
+    youtube_synced = models.DateTimeField(
+        auto_now=False, auto_now_add=False, null=True)
+
+    def __unicode__(self):
+        return u'Playlist item: %s, user: %s' % (self.item_obj.source_identifier,
+                                                self.playlist_obj.user)
 
